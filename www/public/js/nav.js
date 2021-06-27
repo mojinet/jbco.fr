69667f3c4ceb0598d29e6e0c9208fd96de51976b
Modji <jeanbaptiste.cochinard@gmail.com>
2021-06-27 14:44:45 +0200

    UPDATE add switch to switch lightMode

--- www/public/js/nav.js
+++ www/public/js/nav.js
@@ -1,3 +1,6 @@
+/*********************** */
+/*          MENU         */ 
+/*********************** */
 var navIco = document.getElementById("main_nav-icone");
 var navM = document.getElementById("smart_nav");
 
@@ -13,4 +16,54 @@ navIco.addEventListener("click",function(){
 //event sur le menu nav
 navM.addEventListener("click",function(){
     navM.classList.remove("openNav")
+})
+
+/*********************** */
+/*        Switch         */ 
+/*********************** */
+var switchContainerElm = document.getElementById("switch_container");
+var sunElm = document.getElementById("switch_sun");
+var moonElm = document.getElementById("switch_moon");
+var switchElm = document.getElementById("switch_theme");
+var competenceElm = document.getElementById("competences");
+var avatarWElm = document.getElementById("competence_avatar_W");
+var avatarBElm = document.getElementById("competence_avatar_B");
+
+switchContainerElm.addEventListener("click", function(){
+    if (switchElm.classList.contains("switch_push")){
+        switchElm.classList.remove("switch_push");
+        moonElm.classList.remove("display_none");
+        sunElm.classList.add("display_none");
+        competenceElm.classList.remove("l_competencesW")
+        avatarWElm.classList.remove("display_none");
+        avatarBElm.classList.add("display_none");
+        switchContainerElm.classList.remove("switch_black");
+        switchElm.classList.remove("switch_black");
+        document.getElementById("gearB").classList.add("display_none")
+        document.getElementById("monitorB").classList.add("display_none")
+        document.getElementById("paletteB").classList.add("display_none")
+        document.getElementById("conceptB").classList.add("display_none")
+        document.getElementById("gearW").classList.remove("display_none")
+        document.getElementById("monitorW").classList.remove("display_none")
+        document.getElementById("paletteW").classList.remove("display_none")
+        document.getElementById("conceptW").classList.remove("display_none")
+        
+    }else{
+        switchElm.classList.add("switch_push");
+        moonElm.classList.add("display_none");
+        sunElm.classList.remove("display_none");
+        competenceElm.classList.add("l_competencesW")
+        avatarWElm.classList.add("display_none");
+        avatarBElm.classList.remove("display_none");
+        switchContainerElm.classList.add("switch_black");
+        switchElm.classList.add("switch_black");
+        document.getElementById("gearB").classList.remove("display_none")
+        document.getElementById("monitorB").classList.remove("display_none")
+        document.getElementById("paletteB").classList.remove("display_none")
+        document.getElementById("conceptB").classList.remove("display_none")
+        document.getElementById("gearW").classList.add("display_none")
+        document.getElementById("monitorW").classList.add("display_none")
+        document.getElementById("paletteW").classList.add("display_none")
+        document.getElementById("conceptW").classList.add("display_none")
+    }
 })
